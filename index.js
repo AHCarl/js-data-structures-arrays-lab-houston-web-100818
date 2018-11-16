44f3970030b9820d2d3b4525ad8a387614e6f6f8
Alexander Carlin <ahcarlin@gmail.com>
2018-11-16 17:24:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function destructivelyRemoveFirstDriver() {
 
 function appendDriver(name) {
   const copyDrivers = drivers
-  copyDrivers.push(name);
+  return copyDrivers.push(name);
 }
 
 function prependDriver(name) {
